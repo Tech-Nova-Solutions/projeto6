13ba5ebea6e55a86685e31d814b4763d944bdf76
Giulia Barbizan <giuliabarbizan@gmail.com>
2024-09-23 08:35:37 -0300

    criando estrutura do projeto

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+
+const router = createBrowserRouter([
+
+    {
+      path:'/', element:<App/>,
+      errorElement: <Error/>,
+
+      children:[
+        {path:'/', element:<Home/>},
+        {path:'/login', element:<Login/>},
+        {path:'/musicas', element:<Musicas/>},
+
+      ]
+    }
+
+]);
+
+
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router = {router} />
   </StrictMode>,
 )
